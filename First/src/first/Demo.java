ef0079fd23b7f9618b3f899afbf9149687acf73b
mohini <mohiniwaghamare7@gmail.com>
2024-06-05 15:23:05 +0530

    This is a triyal project

---
+++ First/src/first/Demo.java
@@ -0,0 +1,12 @@
+package first;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this my first java project");
+
+	}
+
+}
